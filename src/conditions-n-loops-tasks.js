e4a1fb4bf47236b312b0f5767409758dc594fe0f
TaniDou <t.doump@gmail.com>
2024-01-18 19:32:56 +0300

    feat: Palindrom

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -223,8 +223,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    result += str[str.length - 1 - i];
+  }
+  return str === result;
 }
 
 /**
